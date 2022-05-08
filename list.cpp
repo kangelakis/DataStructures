f2569eca17bde80abbe5ec5815b942ef20c236fc
Nikodimos Skaroulis <96421534+Nikodimos-S@users.noreply.github.com>
2022-05-08 18:48:09 +0300

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -23,6 +23,36 @@ bool search(Node* head, int x)
 {
     Node* current = head; // Initialize current
     while (current != NULL)
+#include <iostream>
+#include <random>
+#include <functional>
+
+using namespace std;
+ 
+// linked list 
+struct node
+{
+   int data;
+   struct node *next;
+   node()
+    {
+        data = 0;
+        next = NULL;
+    }
+};
+//new  node in front
+void push(struct node** head, int node_data)
+{
+   struct node* newnode = new node;
+   newnode->data = node_data;
+   newnode->next = (*head);
+   (*head) = newnode;
+}
+ //Check if element is in list(used in the histogram)
+bool search(node* head, int x)
+{
+    node* current = head; // Initialize current
+    while (current != NULL)
     {
         if (current->data == x)
             return true;
@@ -31,7 +61,7 @@ bool search(Node* head, int x)
     return false;
 }
 // display linked list contents
-void displayList(struct Node *node){
+void displayList(struct node *node){
    while (node != NULL)
    {
       cout<<node->data<<"-->";
@@ -39,10 +69,94 @@ void displayList(struct Node *node){
    }
  
 }
+node * tortoiseAndHareApproach(node *head)
+{
+    node* slow = head;
+    node* fast = head->next;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = (fast->next)->next;
+    }
+    // slow must be pointing at the middle element of the list
+    return slow;
+}
+
+// Function to take two sorted linked lists and merge them
+// to form a sorted resultant linked list
+node * merge(node *left, node *right)
+{
+
+    // creating an auxiliary head node storing the 
+
+    // the head of the linked list to be returned and 
+
+    // another pointer to remember where the last 
+
+    // added node was
+    node *dummyHead = new node();
+    node *current = dummyHead;
+    
+    while (left != NULL && right != NULL)
+    {
+        if (left->data <= right->data)
+        {
+            current-> next = left;
+            left = left->next;
+            current = current-> next;
+        }
+        else if (right->data < left->data)
+        {
+            current->next = right;
+            right = right->next;
+            current = current->next;
+        }
+    }
+    while (left != NULL)
+    {
+        current->next = left;
+        left = left->next;
+        current = current->next;
+    }
+    while (right != NULL)
+    {
+        current->next = right;
+        right = right->next;
+        current = current->next;
+    }
+    return dummyHead->next;
+}
+
+// Function to recursively  divide the linked list
+node * mergeSort(node *start)
+{
+    if (start -> next == NULL)
+    {
+        // only 1 element in current list
+        // so return it as it is
+        return start;
+    }
+    // finding middle of the list using
+    // the tortoise and hare approach
+    node *mid = tortoiseAndHareApproach(start);
+    node *start_of_right = mid->next;
+
+    // breaking the linked list into two parts
+    mid->next = NULL;
+
+    node *left = mergeSort(start);
+    node *right = mergeSort(start_of_right);
+
+    node *new_head = merge(left, right);
+
+    return new_head;
+}
+
+
 //main program 
 int main() {
    // empty list L
-   struct Node* L = NULL;  
+   struct node* L = NULL;  
 
    //Random number gen
    default_random_engine generator;
@@ -58,7 +172,7 @@ int main() {
    } 
 
 //histogram
-struct Node* histogram = NULL;
+struct node* histogram = NULL;
 while (L != NULL)
    {
       int x=L->data;
@@ -70,7 +184,9 @@ while (L != NULL)
       }
    }
 cout<<"Histogram list: "<<endl;
-MergeSort(&histogram);
+displayList(histogram);
+cout<<"Histogram list Sorted: "<<endl;
+histogram=mergeSort(histogram);
 displayList(histogram);
 return 0;
 
