a64dc14cf0c5bff2d5987d9ff7defc0d2e0a5b37
Konstantinos Angelakis <k.aggelakis8@gmail.com>
2022-05-16 17:48:50 +0300

    Update Ex2.cpp

--- 1st Series/2nd exercise/Ex2.cpp
+++ 1st Series/2nd exercise/Ex2.cpp
@@ -1,92 +1,65 @@
 #include <iostream>
 #include <random>
 #include <functional>
+
 using namespace std;
 
 
-void swap(float &x,float &y){
-    float temp = x;
-    x = y;
-    y = temp;
-}
-
-class MinHeap{
+class Heap{
     private:
-
-        float*harr;//pointer to array of elemnts
-        int capacity;//maximum size of heap   
-        int heap_size; //current heap size
+        float *arr;
+        int CurrentSize, MaxSize;
         
     public:
-    
-        MinHeap(int cap){
-            heap_size = 0;
-            capacity = cap;
-            harr = new float[cap];
-        }
-        void printArray(){
-            for(int i=0;i<heap_size;i++){
-                cout<<harr[i]<<endl;
-            }
-            
-        }
-        void insertKey(float k){
-            if(heap_size==capacity){
-                cout<<"Overflow error"<<endl;
-                return;
-            }
-            //Insertnew key at end
-            heap_size++;
-            int i = heap_size - 1;
-            harr[i] = k;
-            while(i!=0&&harr[parent(i)]>harr[1]){
-                swap(harr[i],harr[parent(i)]);
-                i = parent(i);
-            }
-        }
-        int parent(int i) {
-            return (i - 1) / 2;
+        Heap(int MaxSize){ //Constructor Method
+            this->MaxSize = MaxSize; 
+            arr = new float[MaxSize+1];
+            CurrentSize = 0;
         }
 
-        int left(int i) {
-            return (2 * i + 1);
+        float Insert(const float &x){
+            if(CurrentSize == MaxSize){
+                cout << "Heap is full" << endl;
+                return -1;
+            }
+            int i = ++CurrentSize; //Get current size incremented by 1 (new leaf)
+            while(i != 1 && arr[i/2] > x){ 
+                arr[i] = arr[i/2];
+                i /= 2;
+            }
+            arr[i] = x; 
+            return x;
         }
 
-        int right(int i) {
-            return (2 * i + 2);
-        }
-        /*void MinHeapify(int i) {
-            int l = left(i);
-            int r = right(i);
-            int smallest = parent(i);
-            if (l < heap_size && harr[l] < harr[i])
-                smallest = l;
-            if (r < heap_size && harr[r] < harr[smallest])
-                smallest = r;
-            if (smallest != i) {
-                swap(harr[i], harr[smallest]);
-                MinHeapify(smallest);
+        void Display(){
+            for(int i = 1; i <= CurrentSize; i++){
+                cout << arr[i] << endl;
             }
+            cout << endl;
         }
-        */
+
     
 };
 
 int main(){
     //rng 
     default_random_engine generator;
+    uniform_int_distribution<int> data_element_distribution(0, 30);
+    auto random_num = std::bind(data_element_distribution, generator);
+    /*
+    default_random_engine generator;
     chi_squared_distribution<float> my_distribution(0.5);
-    auto random_num = std::bind(my_distribution, generator);
+    auto random_num = std::bind(my_distribution, generator);*/
 
     cout << "Enter the amount of elements: ";
     int n;
     cin >> n;
-    MinHeap *heap_min = new MinHeap(n);
-    for (int i = 0; i < n; i++) {
-        heap_min->insertKey(random_num());
+    Heap *heap_min = new Heap(n);
+    Heap *heap_max = new Heap(n);
+    for (int i = 0; i < n; i++){
+        heap_min->Insert(random_num());
     }
-
-    heap_min->printArray();
+    heap_min->Display();
 
     //system("pause");
     return 0;
